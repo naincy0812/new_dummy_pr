4582836fb7b048d91cda4c542a6ac443615ae4d0
naincy0812 <naincy.chourasia@apphelix.ai>
2025-05-03 00:22:59 +0530

    Update dummy.java

--- dummy.java
+++ dummy.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     // Function name: printHelloWorld
     public static void printHelloWorld() {
         System.out.println("Hello, World");
+        System.out.println("new Hello, World ,Namste");
     }
 
     public static void main(String[] args) {
